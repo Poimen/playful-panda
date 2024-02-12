2836843eb156feb6c3578a3868aff8ff66a1ade2
Richard Shephard <Poimen@users.noreply.github.com>
2024-02-12 16:19:56 +0200

    add redis basics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 mod settings;
 mod short_id;
 
-use settings::ShortIdSettings;
+use redis::Commands;
+use settings::AppSettings;
 
 fn main() {
-    let settings = ShortIdSettings::new();
+    let url: String = String::from("https://www.google.com");
+    let settings = AppSettings::new();
+    let short_id = short_id::generate(&settings.short_id);
 
-    println!("short-id: {}", short_id::generate(&settings));
+    println!("short-id: {}", short_id);
+
+    let client = redis::Client::open(settings.redis.server.as_str());
+    let mut con = client.unwrap().get_connection().unwrap();
+
+    let lookup = format!("url_short:{short_id}");
+    let _: () = con.set(&lookup, url).unwrap();
+
+    let res: bool = con.exists(&lookup).unwrap();
+
+    let new_id: String = con.get(&lookup).unwrap();
+    println!("short-id: {} {}", new_id, res);
 }
--- src/settings.rs
+++ src/settings.rs
@@ -8,12 +8,38 @@ pub struct ShortIdSettings {
     pub id_len: u8,
 }
 
+pub struct RedisClusterSettings {
+    pub server: String,
+}
+
+pub struct AppSettings {
+    pub redis: RedisClusterSettings,
+    pub short_id: ShortIdSettings,
+}
+
 impl ShortIdSettings {
     pub fn new() -> ShortIdSettings {
         // TODO: Get this from Redis
         ShortIdSettings {
             alphabet: Vec::from(SHORT_ID_ALPHABET),
             id_len: SHORT_ID_LEN,
+        }
+    }
+}
+
+impl RedisClusterSettings {
+    pub fn new() -> RedisClusterSettings {
+        RedisClusterSettings {
+            server: String::from("redis://default:changeit@127.0.0.1/"),
+        }
+    }
+}
+
+impl AppSettings {
+    pub fn new() -> AppSettings {
+        AppSettings {
+            redis: RedisClusterSettings::new(),
+            short_id: ShortIdSettings::new(),
         }
     }
 }
